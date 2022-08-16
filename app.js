d4ec6b7c82f3534f1badd68993fafd8fef5f5d5b
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2022-08-15 18:27:58 -0700

    Password hashing.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
+const { rmSync } = require('fs');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const mongoDb = process.env.DB_STRING;
@@ -42,10 +44,13 @@ passport.use(
             if (!user) {
                 return done(null, false, {message: "Incorrect username"});
             }
-            if (user.password !== password) {
-                return done(null, false, {message: "Incorrect password"});
-            }
-            return done(null, user);
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    return done(null, user)
+                } else {
+                    return done(null, false, {message: "Incorrect password"})
+                }
+            })
         });
     })
 );
@@ -65,23 +70,38 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extend: false}));
 
+// This is a middleware function that lets you have access to the 
+// currentUser variable in all of our views without manually passing into the controllers.
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {user: req.user});
 });
 
 app.get("/sign-up", (req, res) => {
     res.render("sign-up-form")
 });
 
-app.post("/sign-up", (req, res, next) => {
-    const user = new User({
+app.post("/sign-up", async (req, res, next) => {
+
+    let user = new User({
         username: req.body.username,
         password: req.body.password
-    }).save(err => {
-        if (err) {
-            return next(err);
-        }
-        res.redirect("/");
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(user.password, salt);
+    
+    user.save().then(response => {
+        res.status(201).redirect("/");
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
     });
 });
 
@@ -92,6 +112,15 @@ app.post(
         failureRedirect: "/"
     })
 );
+
+app.get("/log-out", (req, res) => {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+});
 
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
