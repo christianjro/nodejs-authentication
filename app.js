45a2c0eac0c7a50b864fb8b7026b4b34a9080469
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-03-23 18:38:18 -0700

    refactor to work with mongoose version update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
@@ -10,6 +11,7 @@ const { rmSync } = require('fs');
 const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
+// Connect to MongoDB
 const mongoDb = process.env.DB_STRING;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
@@ -27,58 +29,66 @@ const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
+
 app.use(session({ 
     secret: process.env.SECRET, 
     resave: false, 
     saveUninitialized: true
 }));
 
+app.use(flash());
+
 // Authentication: 3 functions and app.post for the /log-in path
 // Function 1: setting up the LocalStrategy 
 passport.use(
-    new LocalStrategy((username, password, done) => {
-        User.findOne({username: username}, (err, user) => {
-            if (err) {
-                return done(err);
-            }
+    new LocalStrategy(async(username, password, done) => {
+        try {
+            const user = await User.findOne({username: username});
             if (!user) {
                 return done(null, false, {message: "Incorrect username"});
-            }
+            };
+
             bcrypt.compare(password, user.password, (err, res) => {
                 if (res) {
-                    return done(null, user)
+                    return done(null, user);
                 } else {
-                    return done(null, false, {message: "Incorrect password"})
-                }
-            })
-        });
+                    return done(null, false, {message: "Incorrect password"});
+                };
+            });
+
+        } catch(err) {
+            return done(err);
+        };
     })
-);
+)
 
 // Function 2 & 3: Sessions and Serialization
 passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
-});
+passport.deserializeUser(async function(id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch(err) {
+        done(err);
+    }
+})
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extend: false}));
 
-// This is a middleware function that lets you have access to the 
-// currentUser variable in all of our views without manually passing into the controllers.
-app.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    next();
-});
+// // This is a middleware function that lets you have access to the 
+// // currentUser variable in all of our views without manually passing into the controllers.
+// app.use(function(req, res, next) {
+//     res.locals.currentUser = req.user;
+//     next();
+// });
 
 app.get("/", (req, res) => {
-    res.render("index", {user: req.user});
+    res.render("index", {user: req.user, message: req.flash("error")});
 });
 
 app.get("/sign-up", (req, res) => {
@@ -86,31 +96,32 @@ app.get("/sign-up", (req, res) => {
 });
 
 app.post("/sign-up", async (req, res, next) => {
-
-    let user = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
-
-    const salt = await bcrypt.genSalt(10);
-
-    user.password = await bcrypt.hash(user.password, salt);
-    
-    user.save().then(response => {
-        res.status(201).redirect("/");
-    }).catch(error => {
-        res.status(500).json({
-            error: error
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
         });
-    });
+        bcrypt.hash(user.password, 10, async (err, hashedPassword) => {
+            if(err) {
+                return next(err);
+            } else {
+                user.password = hashedPassword;
+                const result = await user.save();
+                res.redirect("/");
+            }
+        })
+    } catch(err) {
+        return next(err)
+    }
 });
 
 app.post(
     "/log-in", 
     passport.authenticate("local", {
         successRedirect: "/",
-        failureRedirect: "/"
-    })
+        failureRedirect: "/",
+        failureFlash: true
+    }),
 );
 
 app.get("/log-out", (req, res) => {
